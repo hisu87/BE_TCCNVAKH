27128e4f7b819c4fa1f360862322dc939ccaaf63
hisu87 <numpadagain@gmail.com>
2024-09-23 12:12:28 +0700

    Go

---
+++ src/main/java/com/datn/be_tccnvakh/BeTccnvakhApplication.java
@@ -0,0 +1,13 @@
+package com.datn.be_tccnvakh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTccnvakhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeTccnvakhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/datn/be_tccnvakh/BeTccnvakhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn.be_tccnvakh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTccnvakhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
